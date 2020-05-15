44b2161ee79caea1d640f11dd2880600c3643e42
Cyrus Jose <cyrus.jose@outlook.com>
2020-05-14 19:13:35 -0700

    Cleaned up the code

--- Class-Examples/classExample.js
+++ Class-Examples/classExample.js
@@ -4,4 +4,4 @@ var nickCharacters = ["Tommy", "Doug", "Oblina"]
 // This will access the last object in the array
 nickCharacters[nickCharacters.length-1]
 // This will access a certain index in the array in this case the first object
-nickCharacters[nickCharacters[0]]
+nickCharacters[0]
